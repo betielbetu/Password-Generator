1d151e7d367943c6114b184a8532bf5acd96236b
System Administrator <root@betiels-MacBook-Pro.local>
2020-12-30 14:43:46 -0600

    added numbers

--- Develop/script.js
+++ Develop/script.js
@@ -15,26 +15,31 @@ function generatePassword() {
   var length = prompt("How long is your password?");
   var includeLowerCase = confirm("would you like to use lowercase?");
   var includeUpperCase = confirm("would you like to use uppercase?");
+  var includeNumbers = confirm ("would you like to use numbers?");
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
 
 
-  while (!includeUpperCase && !includeLowerCase) {
+  while (!includeUpperCase && !includeLowerCase && !includeNumbers) {
     alert("You must select at least one character type!");
     includeUpperCase = confirm("would you like to use uppercase letters?");
     includeLowerCase = confirm("Would you like to use lowercase letters?");
+    includeNumbers = confirm ("would you like to use numbers?")
 
   }
   while (pass.length<length) {
     if (includeLowerCase && pass.length<length) {
       var c = Math.floor(Math.random() * lowercase.length + 1);
       pass += lowercase.charAt(c); 
-      
     }
     if (includeUpperCase && pass.length<length) {
       var c = Math.floor(Math.random() * uppercase.length + 1);
       pass += uppercase.charAt(c); 
-      
+    } 
+    if (includeNumbers && pass.length<length) {
+      var c = Math.floor(Math.random() * numbers.length + 1);
+      pass += numbers.charAt(c); 
     } 
 
     

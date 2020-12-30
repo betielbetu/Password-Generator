c0fc0364c7e37d07de0d2e08baffc520f401fcc6
System Administrator <root@betiels-MacBook-Pro.local>
2020-12-30 14:31:24 -0600

    added uppercase

--- Develop/script.js
+++ Develop/script.js
@@ -13,18 +13,33 @@ function writePassword() {
 function generatePassword() {
   var pass = ''
   var length = prompt("How long is your password?");
-  var includeLowerCase = confirm("do you want to use lowercase?");
-  var lowercase="abcdefghijklmnopqrstuvwxyz";
-  while (length > 0) {
-    if (includeLowerCase === true) {
-      var c = Math.floor(Math.random()*lowercase.length+1);
-      pass+=lowercase.charAt(c);
+  var includeLowerCase = confirm("would you like to use lowercase?");
+  var includeUpperCase = confirm("would you like to use uppercase?");
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+  while (!includeUpperCase && !includeLowerCase) {
+    alert("You must select at least one character type!");
+    includeUpperCase = confirm("would you like to use uppercase letters?");
+    includeLowerCase = confirm("Would you like to use lowercase letters?");
+
+  }
+  while (pass.length<length) {
+    if (includeLowerCase && pass.length<length) {
+      var c = Math.floor(Math.random() * lowercase.length + 1);
+      pass += lowercase.charAt(c); 
+      
     }
-   
-    length--;
+    if (includeUpperCase && pass.length<length) {
+      var c = Math.floor(Math.random() * uppercase.length + 1);
+      pass += uppercase.charAt(c); 
+      
+    } 
+
+    
   }
   return pass;
-
 }
-  // Add event listener to generate button
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
